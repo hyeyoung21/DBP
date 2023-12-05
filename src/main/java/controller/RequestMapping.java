a14f3f7ea2252b71dee50b2f60fd56975b61d82d
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2023-12-05 14:49:45 +0900

    revert

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -5,9 +5,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import controller.user.*;
-import controller.comm.*;
-
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     
@@ -16,31 +13,28 @@ public class RequestMapping {
 
     public void initMapping() {
     	// 각 uri에 대응되는 controller 객체를 생성 및 저장
-        mappings.put("/", new ForwardController("index.jsp"));
-        mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
-        mappings.put("/user/login", new LoginController());
+    	// 기존 폼 입력 처리 개선을 위해 폼 jsp는 컨트롤러에서 redirect함.
+        mappings.put("/", new ForwardController("main.jsp"));
+      
+        mappings.put("/user/login", new LoginController());  
         mappings.put("/user/logout", new LogoutController());
         mappings.put("/user/list", new ListUserController());
         mappings.put("/user/view", new ViewUserController());
         
-        // 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
-//      mappings.put("/user/register/form", new ForwardController("/user/registerForm.jsp"));
-//      mappings.put("/user/register", new RegisterUserController());
-        mappings.put("/user/register", new RegisterUserController());
-
-        // 사용자 정보 수정 폼 요청과 수정 요청 처리 병합
-//      mappings.put("/user/update/form", new UpdateUserFormController());
-//      mappings.put("/user/update", new UpdateUserController());        
+        // 회원 관리
+        mappings.put("/user/add", new AddUserController());
         mappings.put("/user/update", new UpdateUserController());
-        
         mappings.put("/user/delete", new DeleteUserController());
         
-        // 커뮤니티 관련 request URI 추가
-        mappings.put("/community/list", new ListCommunityController());
-        mappings.put("/community/view", new ViewCommunityController());
-        mappings.put("/community/create/form", new ForwardController("/community/creationForm.jsp"));
-        mappings.put("/community/create", new CreateCommunityController());
-        mappings.put("/community/update", new UpdateCommunityController());
+        // 작성글 관리
+        mappings.put("/post/view", new PostViewontroller()); // 작성글 조회
+        mappings.put("/post/add", new PostAddController()); // 작성글 등록
+        mappings.put("/post/delete", new PostDeleteController()); // 작성글 삭제
+        mappings.put("/post/update", new PostUpdateController()); // 작성글 수정
+        
+        // 신청글 조회
+        mappings.put("/applyPost/view", new ApplyPostViewController()); // 신청글 등록
+        mappings.put("/applyPost", new ApplyPostDeleteController()); // 신청글 삭제
         
         logger.info("Initialized Request Mapping!");
     }
