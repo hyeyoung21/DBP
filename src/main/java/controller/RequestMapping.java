a98543f147951580109919a85f3e482d324642b6
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2023-12-21 21:09:46 +0900

    switch comment

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -35,7 +35,7 @@ public class RequestMapping {
         //마이페이지
         mappings.put("/mypage/mypage",new MyPageController());
         mappings.put("/mypage/calendar",new CalendarController());
-        mappings.put("/user/myComment",new ListMyCommentController());
+        mappings.put("/mypage/myComment",new ListMyCommentController());
         
         // 작성글 관리
         mappings.put("/post/list", new ListPostController()); // 작성글 목록
