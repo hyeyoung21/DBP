0de5114b08da945d5e2212c47ea746622b7ccb8c
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2023-12-23 00:04:52 +0900

    .

--- src/main/java/controller/post/DeleteCommentController.java
+++ src/main/java/controller/post/DeleteCommentController.java
@@ -9,9 +9,10 @@ import model.service.PostManager;
 public class DeleteCommentController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("delete");
+        
         PostManager manager = PostManager.getInstance();
         int commentID = Integer.parseInt(request.getParameter("commentID"));
+        
         manager.deleteComment(commentID);
         return "redirect:/post/list";
     }
